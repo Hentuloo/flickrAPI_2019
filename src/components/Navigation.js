f6d7abcfdda4764bc29b119c5d6fb8a23d16db97
Kamil <Kamil@>
2019-12-03 19:01:59 +0100

    create bar and person circle

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Constants from 'config/Constants';
 
+import MarilynMonroeImage from 'assets/images/MarilynMonroe.jpg';
+import styles from './Navigation.module.scss';
+
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={Constants.PATHS.root.path}>
-            {Constants.PATHS.root.name}
-          </Link>
-        </li>
-        <li>
-          <Link to={Constants.PATHS.gallery.path}>
-            {Constants.PATHS.gallery.name}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navigation}>
+      <div className={styles.navigation__person}>
+        <img
+          className={styles.navigation__person_image}
+          src={MarilynMonroeImage}
+          alt="Fotografia Marilyn"
+        />
+      </div>
+      <nav className={styles.navigation__list}>
+        <ul>
+          <li>
+            <Link to={Constants.PATHS.root.path}>
+              {Constants.PATHS.root.name}
+            </Link>
+          </li>
+          <li>
+            <Link to={Constants.PATHS.gallery.path}>
+              {Constants.PATHS.gallery.name}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
