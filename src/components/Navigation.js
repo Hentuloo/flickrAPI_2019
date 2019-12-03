b641f8a77e30adfddfe25df06ca9ee7a481f1d50
Kamil <Kamil@>
2019-12-03 19:51:00 +0100

    add more responsiveness and correct navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 import Constants from 'config/Constants';
 
 import MarilynMonroeImage from 'assets/images/MarilynMonroe.jpg';
-import styles from './Navigation.module.scss';
+import styles, {
+  navigation,
+  navigation__person,
+  navigation__person_image,
+  navigation__nav,
+  navigation__nav_list,
+  navigation__nav_element,
+  navigation__nav_link,
+} from './Navigation.module.scss';
 
 const Navigation = () => {
   return (
-    <div className={styles.navigation}>
-      <div className={styles.navigation__person}>
+    <div className={navigation}>
+      <div className={navigation__person}>
         <img
-          className={styles.navigation__person_image}
+          className={navigation__person_image}
           src={MarilynMonroeImage}
           alt="Fotografia Marilyn"
         />
       </div>
-      <nav className={styles.navigation__list}>
-        <ul>
-          <li>
-            <Link to={Constants.PATHS.root.path}>
-              {Constants.PATHS.root.name}
-            </Link>
+      <nav className={navigation__nav}>
+        <ul className={navigation__nav_list}>
+          <li className={navigation__nav_element}>
+            <NavLink
+              className={navigation__nav_link}
+              to={Constants.PATHS.root.path}
+              activeClassName={styles['navigation__nav_link--active']}
+              exact
+            >
+              <span className="sr-only">
+                {Constants.PATHS.root.name}
+              </span>
+              <i className="fa fa-user" aria-hidden="true" />
+            </NavLink>
           </li>
-          <li>
-            <Link to={Constants.PATHS.gallery.path}>
-              {Constants.PATHS.gallery.name}
-            </Link>
+          <li className={navigation__nav_element}>
+            <NavLink
+              className={navigation__nav_link}
+              to={Constants.PATHS.gallery.path}
+              activeClassName={styles['navigation__nav_link--active']}
+            >
+              <span className="sr-only">
+                {Constants.PATHS.gallery.name}
+              </span>
+              <i className="fa fa-picture-o" aria-hidden="true" />
+            </NavLink>
           </li>
         </ul>
       </nav>
