4139c7ed9450359216c3d1f2f7092bf59ddb7dc0
Kamil <Kamil@>
2019-12-05 09:04:11 +0100

    correct navigation to content dimensions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,46 +11,48 @@ import './Navigation.scss';
 const Navigation = ({ className }) => {
   return (
     <div className={cx(className, 'navigation')}>
-      <div className="navigation__person">
-        <img
-          className="navigation__person-image"
-          src={MarilynMonroeImage}
-          alt="Fotografia Marilyn"
-        />
+      <div className="navigation__wrapper">
+        <div className="navigation__person">
+          <img
+            className="navigation__person-image"
+            src={MarilynMonroeImage}
+            alt="Fotografia Marilyn"
+          />
+        </div>
+        <nav className="navigation__nav">
+          <ul className="navigation__nav-list">
+            <li className="navigation__nav-element">
+              <NavLink
+                className="navigation__link"
+                to={Constants.PATHS.root.path}
+                activeClassName="navigation__link navigation__link--active"
+                exact
+              >
+                <div className="navigation__link-inner">
+                  <span className="sr-only">
+                    {Constants.PATHS.root.name}
+                  </span>
+                  <i className="fa fa-user" aria-hidden="true" />
+                </div>
+              </NavLink>
+            </li>
+            <li className="navigation__nav-element">
+              <NavLink
+                className="navigation__link"
+                to={Constants.PATHS.gallery.path}
+                activeClassName="navigation__link navigation__link--active"
+              >
+                <div className="navigation__link-inner">
+                  <span className="sr-only">
+                    {Constants.PATHS.gallery.name}
+                  </span>
+                  <i className="fa fa-picture-o" aria-hidden="true" />
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="navigation__nav">
-        <ul className="navigation__nav-list">
-          <li className="navigation__nav-element">
-            <NavLink
-              className="navigation__link"
-              to={Constants.PATHS.root.path}
-              activeClassName="navigation__link navigation__link--active"
-              exact
-            >
-              <div className="navigation__link-inner">
-                <span className="sr-only">
-                  {Constants.PATHS.root.name}
-                </span>
-                <i className="fa fa-user" aria-hidden="true" />
-              </div>
-            </NavLink>
-          </li>
-          <li className="navigation__nav-element">
-            <NavLink
-              className="navigation__link"
-              to={Constants.PATHS.gallery.path}
-              activeClassName="navigation__link navigation__link--active"
-            >
-              <div className="navigation__link-inner">
-                <span className="sr-only">
-                  {Constants.PATHS.gallery.name}
-                </span>
-                <i className="fa fa-picture-o" aria-hidden="true" />
-              </div>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
